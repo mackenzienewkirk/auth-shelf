451db6233fd98001f024cc12e8368db4d9bd493f
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-02 10:26:17 -0600

    saga function fetchItems

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,27 +6,27 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 /**
  * Get all of the items on the shelf
  */
-router.get('/', rejectUnauthenticated, (req, res) => {
-  const currentUserID = req.user.id;
+// router.get('/', rejectUnauthenticated, (req, res) => {
+//   const currentUserID = req.user.id;
 
-  const sqlQuery = `
-  SELECT * FROM "item"
-  WHERE "user_id"=$1
-  ORDER BY "id";
-  `
+//   const sqlQuery = `
+//   SELECT * FROM "item"
+//   WHERE "user_id"=$1
+//   ORDER BY "id";
+//   `
 
-  const sqlValues = [currentUserId];
+//   const sqlValues = [currentUserID];
 
-  pool.query(sqlQuery, sqlValues)
-    .then((dbRes) => {
-      res.send(dbRes.rows);
-    })
-    .catch((dbErr) => {
-      console.log('GET things failed:', dbErr);
-      res.sendStatus(500);
-    })
-     // For testing only, can be removed
-});
+//   pool.query(sqlQuery, sqlValues)
+//     .then((dbRes) => {
+//       res.send(dbRes.rows);
+//     })
+//     .catch((dbErr) => {
+//       console.log('GET things failed:', dbErr);
+//       res.sendStatus(500);
+//     })
+//      // For testing only, can be removed
+// });
 
 /**
  * Add an item for the logged in user to the shelf
@@ -60,8 +60,31 @@ router.get('/count', (req, res) => {
 /**
  * Return a specific item by id
  */
-router.get('/:id', (req, res) => {
-  // endpoint functionality
+
+
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+  const currentUserID = req.user.id;
+
+  const sqlQuery = `
+  SELECT * FROM "item"
+  WHERE "user_id"=$1
+  ORDER BY "id";
+  `
+
+  const sqlValues = [currentUserID];
+
+  pool.query(sqlQuery, sqlValues)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((dbErr) => {
+      console.log('GET things failed:', dbErr);
+      res.sendStatus(500);
+    })
+     // For testing only, can be removed
 });
+// router.get('/:id', (req, res) => {
+//   // endpoint functionality
+// });
 
 module.exports = router;
---
+++ src/redux/sagas/item.saga.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchItems() {
+    try {
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+
+        const response = yield axios.get('/api/shelf', config);
+
+        yield put({ type: 'SET_ITEM', payload: response.data });
+    } catch (error) {
+        console.log('could not find', error);
+    }
+}
+
+function* itemSaga() {
+    yield takeLatest('FETCH_ITEM', fetchItems);
+}
+
+export default fetchItems; 
