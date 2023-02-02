bfb673ebbca4ed372c4557c49b7590dd915bf17c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-02-02 11:18:58 -0600

    fixed root saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -19,7 +19,7 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
-    addToListSaga()
+    addToListSaga(),
 
     itemSaga()
 
